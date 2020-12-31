36ff9dfa120dbdf1dda77b8e02ca2f6f6a17537d
Claudio <lapertosa.c@gmail.com>
2020-12-31 12:58:41 +0100

    Clean Architecture: folders and projects

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace GamesDB.API
+namespace API
 {
     public class Startup
     {
