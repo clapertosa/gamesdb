2aec8d42187c9d7a1497ee2e30a160e51c61657c
Claudio <lapertosa.c@gmail.com>
2021-01-01 02:05:03 +0100

    Dotnet Secrets and Environment Variables test (secret dev/env vars production)

---
+++ WebUI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebUI.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        [HttpGet]
+        public string Index()
+        {
+            return _configuration["IGDB_CLIENT_ID"];
+        }
+    }
+}
